dee983f9b28c763d89bb2e3e2b5d6af3b9798239
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-05-25 16:30:11 -0400

    created mobile app for restaurants api

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, ImageBackground, ScrollView } from 'react-native';
+import { useEffect, useState, } from 'react';
+
+const image = { uri: 'https://c8.alamy.com/comp/M9DH04/waiter-wearing-the-uniform-holding-a-dish-of-chicken-cartoon-character-set-of-fun-flat-cartoon-person-M9DH04.jpg' }
 
 export default function App() {
+  const [allRestaurants, setAllRestaurants] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch('https://my-first-firestore-lm.web.app/restaurants');
+        const data = await response.json();
+        setAllRestaurants(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getData();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground resizeMode='cover' source={image} style={styles.container}>
+        <ScrollView>
+          {allRestaurants ? (
+            allRestaurants?.map(singleRest => (
+              <Text style={styles.restaurantsName} key={singleRest.id}>
+                {singleRest.name}
+              </Text>
+            ))
+          ) : (
+            <ActivityIndicator size='large' color='red' />
+          )}
+        </ScrollView>
+        <Text styl={{ color: 'white', marginBottom: 20 }}>Welcome to our many restuarant options provided </Text>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -16,5 +48,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  restaurantsName: {
+    color: 'teal',
+    fontSize: 65,
+    marginVertical: 150
   },
 });
